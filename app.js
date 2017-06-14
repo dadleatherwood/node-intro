aeb1f53bfbe1e59f7c0191432fdebafbd49731a4
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-06-14 12:30:57 -0500

    completed day 2

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var messagesCtrl = require('./controllers')
+var messages = require('./messages')
+var app = express();
+
+
+
+
+//app.use(function(req,res,next))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//'use' function to be used for specific purposes instead of ALL cases
+var addMessageID = function(req,res,next){
+  console.log('this is the middleware');
+  var id = messages.length;
+  req.body.id = id
+  req.body.category = id % 2 == 0 ? 'even' : 'odd'
+  next();
+}
+
+
+//app.get(endpoint, callback)
+app.get('/messages', messagesCtrl.getMessages)
+
+//called 'addMessageID' by putting it between the name and function
+app.post('/messages', addMessageID, messagesCtrl.postMessages)
+
+app.get('/messages/:id', messagesCtrl.getMessage)
+app.get('/getMessageByCategory',messagesCtrl.getByCategory)
+
+
+app.listen(3000, function(){
+  console.log('Example app listening on port 3000')
+})
---
+++ controllers.js
@@ -0,0 +1,27 @@
+var messages = require('./messages')
+
+module.exports = {
+  getMessages: function(req,res){
+    res.send(messages)
+  },
+  postMessages: function(req,res){
+    console.log('this is the post endpoint')
+    var message = req.body
+    messages.push(message)
+    res.json(messages)
+  },
+  getMessage: function(req,res){
+    var id = req.params.id
+    var message = messages.find(function(message){
+      return message.id == id
+    })
+    res.json(message)
+  },
+  getByCategory: function(req,res){
+    var category = req.query.category
+    var filtered = messages.filter(function(message){
+      return message.category == category
+    })
+    res.json(filtered)
+  }
+}
